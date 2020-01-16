6506b4841d11cb4b58b40ade78c1c855c78dd80d
Christopher M Jones <chjones@idmod.org>
2020-01-16 15:25:44 -0800

    Attempt uploading an artifact.

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
 
 sh -c "echo -------------------------------------------"
-sh -c "echo ==== package versions ===="
-sh -c "python3.6 --version"
-sh -c "pip3.6 freeze"
+sh -c "pip3.6 freeze > /requirements.txt"
